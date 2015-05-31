e948be97cb9f7b6130b107dd24c69e062c68b65d
robjc <robert.jcb.cole@gmail.com>
2015-05-31 18:00:42 +0000

    added topic actions and views

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,12 +8,11 @@ class IncomingController < ActionController
         @user = User.where(email: params['sender']).first
          
         if @user
-            @topic = Topic.where(name: params[:subject]).first_or_create
-            @topic.bookmarks.create(link: params["stripped-text"], user_id: @user.id)
+            @topic = Topic.where(title: params[:subject]).first_or_create
+            @topic.bookmarks.create(url: params["stripped-text"], user_id: @user.id)
         end
-     
-
-     # TO-DO: 1) Search body of email for url, 2) allow for multiple urls
+        
+        # TO-DO: 1) Search body of email for url, 2) allow for multiple urls
         
         head 200
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :topics
+
   get 'about' => 'welcome#about'
   
   post :incoming, to: 'incoming#create'
