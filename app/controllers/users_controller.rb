7bde5f25501f1dad8139ac0d2a6e26424fa4a2b8
robjc <robert.jcb.cole@gmail.com>
2015-08-01 20:29:14 +0000

    fixed profiles so likes are more efficiently loaded

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @bookmarks = @user.bookmarks
-    #@liked_bookmarks = @user.likes.map { |like| like.bookmark }
-    @liked_bookmarks = User.includes(:likes).where("likes.id").references(:likes).find(current_user)
-    #@liked_bookmarks = Like.includes(:bookmark).where("likes.current_user = true").references(:likes)
+    @liked_bookmarks = Bookmark.includes(:likes).where(likes: {user_id: @user})
   end
 
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,7 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   has_many :likes, dependent: :destroy
   
-  default_scope { order('created_at DESC') }
+  default_scope { order('bookmarks.created_at DESC') }
   
   validates :url, presence: true
   validates :user, presence: true
